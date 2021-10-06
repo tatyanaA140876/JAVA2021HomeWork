542b8f8da1db672d59e0fd67dd48f3a27f96039c
tatyana <androsova14081976@gmail.com>
2021-10-06 10:51:14 -0400

    домашнее задание 5

---
+++ src/lesson6/Cat.java
@@ -0,0 +1,8 @@
+package lesson6;
+
+class Cat extends Animal {
+
+    Cat(double animalDistanceRun, double animalDistanceJump, double animalDistanceSwim) {
+        super(animalDistanceRun, animalDistanceJump, animalDistanceSwim);
+    }
+}
